310abdb98d0c95740e6c00113191c5d805656b78
manucastrillonm <manu.cm43@gmail.com>
2017-05-18 01:23:13 -0500

    Creacion de componente para agregar y editar recetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { RecipesProvider } from '../providers/recipes/recipes';
+import { RecipeFormComponent } from '../components/recipe-form/recipe-form';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    TabsPage
+    TabsPage,
+    RecipeFormComponent
   ],
   imports: [
     BrowserModule,
