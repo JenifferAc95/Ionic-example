f7ac44032bfdd003176a32353a991041c19861d2
manucastrillonm <manu.cm43@gmail.com>
2017-05-18 22:05:58 -0500

    comento lineas de código nuevas

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { Recipe, RecipesProvider } from '../../providers/recipes/recipes'
+// import { Recipe, RecipesProvider } from '../../providers/recipes/recipes';
 
 @IonicPage()
 @Component({
@@ -9,26 +9,33 @@ import { Recipe, RecipesProvider } from '../../providers/recipes/recipes'
 })
 export class RecipesPage {
 
-  recipes: Recipe[];
+  // recipes: Recipe[];
 
-  constructor(public navCtrl: NavController,
-    public recipeService: RecipesProvider) {
-  }
+  /*
+    constructor(public navCtrl: NavController,
+      public recipeService: RecipesProvider) {
+    }
+  */
+
+  //Comment or delete this line
+  constructor(public navCtrl: NavController) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecipesPage');
   }
 
-  ngOnInit() {
-    this.recipeService.getRecipes().then(
-      (recipes) => {
-        console.log(recipes);
-        this.recipes = recipes;
-      },
-      (err) => {
-        console.log("Oops something is wrong!!");
-      }
-    );
-  }
+  /*
+    ngOnInit() {
+      this.recipeService.getRecipes().then(
+        (recipes) => {
+          console.log(recipes);
+          this.recipes = recipes;
+        },
+        (err) => {
+          console.log("Oops something is wrong!!");
+        }
+      );
+    }
+  */
 
 }
--- src/providers/recipes/recipes.ts
+++ src/providers/recipes/recipes.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
+/*
 export class Recipe {
   constructor(
     public id?: number,
@@ -9,10 +10,12 @@ export class Recipe {
     public instructions?: string
   ) { }
 }
+*/
 
-const RECIPES: Recipe[] = [];
-const FETCH_LATENCY = 500;
+// const RECIPES: Recipe[] = [];
+// const FETCH_LATENCY = 500;
 
+/*
 let recipe1: Recipe = new Recipe(1, "Recipe 1", "Ingredient 1, Ingredient 2, Ingredient 3","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 let recipe2: Recipe = new Recipe(2, "Recipe 2", "Ingredient 1, Ingredient 2, Ingredient 3","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 let recipe3: Recipe = new Recipe(3, "Recipe 3", "Ingredient 1, Ingredient 2, Ingredient 3","Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
@@ -28,6 +31,7 @@ RECIPES.push(recipe4);
 RECIPES.push(recipe5);
 RECIPES.push(recipe6);
 RECIPES.push(recipe7);
+*/
 
 @Injectable()
 export class RecipesProvider {
@@ -36,31 +40,38 @@ export class RecipesProvider {
     console.log('Hello RecipesProvider Provider');
   }
 
-  addRecipe(recipe): void {
-    let recipeId = RECIPES.reduce((max, recipe) => Math.max(recipe.id, max), -1) + 1;
-    recipe.id = recipeId;
-    RECIPES.push(recipe);
-  }
+  /*
+    addRecipe(recipe): void {
+      let recipeId = RECIPES.reduce((max, recipe) => Math.max(recipe.id, max), -1) + 1;
+      recipe.id = recipeId;
+      RECIPES.push(recipe);
+    }
+  */
 
-  getRecipes(): Promise<Recipe[]> {
-    return new Promise<Recipe[]>(resolve => {
-      setTimeout(() => {
-        resolve(RECIPES);
-      }, FETCH_LATENCY);
-    })
-  }
+  /*
+    getRecipes(): Promise<Recipe[]> {
+      return new Promise<Recipe[]>(resolve => {
+        setTimeout(() => {
+          resolve(RECIPES);
+        }, FETCH_LATENCY);
+      })
+    }
+  */
 
-  getRecipe(id: number | string): Promise<Recipe> {
-    return new Promise<Recipe>(resolve => {
-      setTimeout(() => {
-        resolve(RECIPES.filter(recipe => recipe.id === +id)[0]);
-      }, FETCH_LATENCY);
-    })
-  }
+  /*
+    getRecipe(id: number | string): Promise<Recipe> {
+      return new Promise<Recipe>(resolve => {
+        setTimeout(() => {
+          resolve(RECIPES.filter(recipe => recipe.id === +id)[0]);
+        }, FETCH_LATENCY);
+      })
+    }
+  */
 
-  removeRecipe(id: number | string): void {
-    let recipeIdx = +id - 1;
-    RECIPES.splice(recipeIdx, 1);
-  }
-
+  /*
+    removeRecipe(id: number | string): void {
+      let recipeIdx = +id - 1;
+      RECIPES.splice(recipeIdx, 1);
+    }
+  */
 }
